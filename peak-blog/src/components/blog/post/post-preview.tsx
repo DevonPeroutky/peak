98dc5382b36754c7ab0e3e16b06569695d843e75
DevonPeroutky <devonperoutky@gmail.com>
2021-06-13 15:31:52 -0700

    [Blog] Handle arbitrary external image urls

--- peak-blog/src/components/blog/post/post-preview.tsx
+++ peak-blog/src/components/blog/post/post-preview.tsx
@@ -5,6 +5,8 @@ import {Node} from "slate";
 import Link from "next/link";
 import moment from "moment";
 import {EstimateReadTime} from "./read-time/EstimatedReadTime";
+import Image from "next/image";
+import {ImageProps} from "next/dist/client/image";
 
 export const BlogPostPreview = (props: { post: PeakPost }) => {
     const { post } = props
@@ -15,16 +17,40 @@ export const BlogPostPreview = (props: { post: PeakPost }) => {
     const title = Node.string(titleNode)
 
     return (
-        <div className={"mb-20"}>
-            <Link href={`post/${post.id}`}>
-                <h1 className={"mb-2 cursor-pointer hover:text-blue-500 font-semibold text-4xl"}>{title}</h1>
-            </Link>
-            <div className={"mb-2 text-lg text-gray-500 font-light"}>
-                {post.subtitle}
-            </div>
-            <div className={"text-base text-gray-400 font-light text-sm mb-4"}>
-                <span className={"mr-1"}>{author.given_name} {author.family_name}</span> • <span className={"mx-1"}>{moment(post.created_at).format('LL') }</span> • <EstimateReadTime className={"mx-1"} body={post.body}/>
+        <div className={"mb-20 flex min-h-150"}>
+            <ImagePreview post={post}/>
+            <div className="flex flex-col flex-grow">
+                <Link href={`post/${post.id}`}>
+                    <h2 className={"mb-2 cursor-pointer hover:text-blue-500 font-semibold text-3xl"}>{title}</h2>
+                </Link>
+                <div className={"mb-2 text-base text-gray-500 font-light flex-grow"}>
+                    {post.subtitle}
+                </div>
+                <div className={"text-gray-400 font-light text-xs mb-4"}>
+                    <span className={"mr-1"}>{author.given_name} {author.family_name}</span> • <span className={"mx-1"}>{moment(post.created_at).format('LL') }</span> • <EstimateReadTime className={"mx-1"} body={post.body}/>
+                </div>
             </div>
         </div>
     )
+}
+
+const ImagePreview = (props: { post: PeakPost }) => {
+    const cover_image = props.post.cover_image
+    return (!cover_image) ? null : <div className={"mr-5"}>
+        <NextImageWrapper src={cover_image} width={"200px"} height={"125px"} layout={"intrinsic"} alt={"Image Preview"}/>
+    </div>
+}
+
+const NextImageWrapper = (props: ImageProps) => {
+    // @ts-ignore
+    const { src, className, alt, width, layout, height } = props
+    return (src.startsWith("https://storage.googleapis.com")) ?
+        <Image
+            src={src}
+            alt={alt}
+            width={width}
+            layout={layout}
+            height={height}
+        /> : <img src={src} className={props.className} alt={alt} style={{width: width, height: height}}/>
+
 }
