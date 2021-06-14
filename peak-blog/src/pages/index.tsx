7120d0f705628a6da057f7223a1ef21b39831c4c
DevonPeroutky <devonperoutky@gmail.com>
2021-06-13 18:01:10 -0700

    [Blog] "A peak blog by..."

--- peak-blog/src/pages/index.tsx
+++ peak-blog/src/pages/index.tsx
@@ -4,12 +4,13 @@ import {InitialLoader} from "../components/loaders/InitialLoader";
 import {useAppContext} from "../data/context";
 
 const App = () => {
-    const { subdomain } = useAppContext()
+    const { subdomain, author } = useAppContext()
 
     return (
         <>
             {/* @ts-ignore */}
             {(subdomain) ? <BlogHome/> : <InitialLoader/>}
+            <div className="fixed bottom-3 left-3 text-gray-500 text-left text-xs flex flex-col">A Peak blog by <span className={"text-base font-bold"}>{author.given_name} {author.family_name}</span></div>
         </>
     )
 }
